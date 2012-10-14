d4703d08adfc3b9eb0b98e56c038f9d3a0876f60
Christopher Lenart <clenart1@icloud.com>
2012-10-14 13:13:30 -0500

    added << overload to game.cpp

--- game.cpp
+++ game.cpp
@@ -18,3 +18,27 @@ using namespace FIAR;
 void Game::calcStatus(const int& r, const int& c) const {
 
 }
+
+std::ostream& operator<<(std::ostream& os, Game& bo) {
+  COLOR* pieces = bo.board.proto_board;
+	os << "\r;  1 2 3 4 5 6 7 8 9 a b c d e f";
+	os << "\r\n";
+	for(int row = 0; row<15; row++) {
+		os << "\r";
+		if(row<9) os << ";" << row+1 << " ";
+		else if(row == 9) os << ";a ";
+		else if (row == 10) os << ";b ";
+		else if (row == 11) os << ";c ";
+		else if (row == 12) os << ";d ";
+		else if (row == 13) os << ";e ";
+		else if (row == 14) os << ";f ";
+		for(int column = 0; column<15; column++) {
+			if(*pieces==EMPTY) os << "+ ";
+			else if(*pieces==WHITE) os << "O ";
+			else os << "@ ";
+			pieces++;
+		}
+		os << "\r\n";
+	}
+	return os;
+}
