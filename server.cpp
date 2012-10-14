afce5c2b65b657b9ef00338e122b89c8c2c1fb76
Christopher Lenart <clenart1@icloud.com>
2012-10-13 21:24:46 -0500

    moved some code to the server class

--- server.cpp
+++ server.cpp
@@ -29,12 +29,114 @@ using boost::asio::ip::tcp;
 
 bool is_hex(const char& c);
 
+enum Difficulty {
+	EASY = 0,
+	MEDIUM,
+	HARD
+};
+
+enum Hex {
+	0 = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F
+};
+
 class Server {
 public:
 	Server(int p) {port = p;}
+
+	voiid server_loop();
+
 private:
 	int port;
+	bool display;
 
+	void set_difficulty(Difficulty);
+	void toggle_display();
+	void undo();
+	void move(Hex, Hex);
+	void comment(std::string);
+}
+
+void Server::server_loop() {
+try
+	{
+		boost::array<char, 128> buf;
+		boost::asio::io_service io_service;
+		tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), port));
+		for(;;) {
+      			tcp::socket socket(io_service);
+      			acceptor.accept(socket);
+
+      			FIAR::Game server_game;
+      			boost::asio::write(socket,boost::asio::buffer("WELCOME\n"));
+      			bool first_go = true;
+      			display = false;
+			bool is_connected = true;
+			while(is_connected) {
+				if(display) {
+					std::stringstream board;
+					board << server_game;
+					std::cout << board.str();
+					boost::asio::write(socket, boost::asio::buffer(board.str().c_str()));
+				}
+
+				// reads data from the socket
+				size_t len = socket.read_some(boost::asio::buffer(buf));
+				// stores the message in client_message
+				std::string client_message = "";
+				for(int i = 0; i<len; i++) {
+					if(buf[i]!=' ') client_message += buf[i];
+				}
+				if(client_message.size()!=0) {
+					// ignores the first message, seems to be erroneous?
+					// should figure out why
+					if(!first_go) std::cout << client_message << std::endl;
+        				else first_go = false;
+					// transforms client_message to lowercase
+					std::transform(client_message.begin(), client_message.end(), client_message.begin(), ::tolower);
+					if(client_message=="exit") {
+						is_connected=false;
+						boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
+					}
+					else if(client_message=="display") {
+						display_toggle();
+						boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
+        				}
+					else if(client_message=="easy") {
+						set_difficulty(EASY);
+						boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
+					}
+					else if(client_message=="medium") {
+						set_difficulty(MEDIUM);
+						boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
+					}
+					else if(client_message=="hard") {
+						set_difficulty(HARD);
+						boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
+					}
+					else if(client_message=="undo") {
+						undo();
+						boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
+					}
+					else if(client_message.size()==2&&is_hex(client_message[0])&&is_hex(client_message[1])) {
+						boost::asio::write(socket, boost::asio::buffer("\rOK, accepted hex move\r\n"));
+					}
+					/*else {
+						boost::asio::write(socket, boost::asio::buffer("\rInvalid command\r\n"));
+					}*/
+				}
+			}
+		}
+	
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void toggle_display() {
+	if(display) display = false;
+	else display = true;;
 }
 
 int main(int argc, char* argv[]) {
@@ -54,69 +156,9 @@ int main(int argc, char* argv[]) {
       return EXIT_FAILURE;
     }
   }
-	try
-	{
-		boost::array<char, 128> buf;
-		boost::asio::io_service io_service;
-		tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), port));
-		for(;;) {
-      			tcp::socket socket(io_service);
-      			acceptor.accept(socket);
+	Server serv(port);
+	serv.server_loop();
 
-      			FIAR::Game server_game;
-      			boost::asio::write(socket,boost::asio::buffer("WELCOME\n"));
-      			bool first_go = true;
-      			bool display = false;
-			bool is_connected = true;
-			while(is_connected) {
-				size_t len = socket.read_some(boost::asio::buffer(buf));
-				std::string client_message = "";
-				for(int i = 0; i<len; i++) {
-					if(buf[i]!=' ') client_message += buf[i];
-				}
-				if(client_message.size()!=0) {
-				if(!first_go) std::cout << client_message << std::endl;
-        			else first_go = false;
-				std::transform(client_message.begin(), client_message.end(), client_message.begin(), ::tolower);
-				if(client_message=="exit") {
-					is_connected=false;
-					boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
-				}
-				else if(client_message=="display") {
-					boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
-					if(display) display = false;
-					else display = true;;
-					std::stringstream board;
-					board << server_game;
-					std::cout << board.str();
-					boost::asio::write(socket, boost::asio::buffer(board.str().c_str()));
-        			}
-				else if(client_message=="easy") {
-					boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
-				}
-				else if(client_message=="medium") {
-					boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
-				}
-				else if(client_message=="hard") {
-					boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
-				}
-				else if(client_message=="undo") {
-					boost::asio::write(socket, boost::asio::buffer("\rOK\r\n"));
-				}
-				else if(client_message.size()==2&&is_hex(client_message[0])&&is_hex(client_message[1])) {
-					boost::asio::write(socket, boost::asio::buffer("\rOK, accepted hex move\r\n"));
-				}
-				else {
-					boost::asio::write(socket, boost::asio::buffer("\rInvalid command\r\n"));
-				}
-			}}
-		}
-
-	}
-  catch(std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
   return EXIT_SUCCESS;
 }
 
