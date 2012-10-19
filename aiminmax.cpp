82c918331d7a6b2768681cbd1af063f9e5708669
Christopher Lenart <clenart1@icloud.com>
2012-10-19 03:41:06 +0200

    implemented a bunch of tree functions

--- aiminmax.cpp
+++ aiminmax.cpp
@@ -12,8 +12,7 @@ Move AIMinMax::getMove(const Board& b) {
 		for(int i = 0; i < head->number_of_children(); i++) {
 			const TreeNode* child = head.get_child(i);
 			if(child.get_board()==b) {
-				// make this
-				pruneTree(child);
+				move_tree.pruneTree(child);
 				found_child = true;
 				break;
 			}
@@ -21,30 +20,30 @@ Move AIMinMax::getMove(const Board& b) {
 		// if no child was found
 		if(!found_child) {
 			delete tree;
-			// make this
-			createTree(b);
+			move_tree.createTree(b);
 		}
 		// if a child was found
 		else {
-			// make this
-			extendTree();
+			move_tree.extendTree();
 		}
-		recalculateTree();
+		move_tree.calculateTree();
 	}
 	// if the tree does not exist
 	else {
-		createTree(b);
+		move_tree.createTree(b);
 	}
 }
 
 class Tree {
 public:
 	Tree();
+	Tree(TreeNode* h) {head = new TreeNode(h->get_board_copy(), h->get_node_type(), h->get_move()); }
 	~Tree() { delete head; }
 
 	const TreeNode* get_head() { return head; }
 	void createTree(const Board& h, int depth);
-
+	void extendTree();
+	void pruneTree(TreeNode* child);
 private:
 	TreeNode* head;
 };
@@ -52,15 +51,21 @@ private:
 class TreeNode {
 public:
 	TreeNode();
+	TreeNode(TreeNode* h);
 	TreeNode(Board, NodeType);
 	TreeNode(Board, NodeType, Move);
 	~TreeNode();
 
-	int number_of_children() { return children.size(); }
-	const TreeNode* get_child(int i) { return children[i]; }
-	const& Board get_board() { return Board; }
+	int 		number_of_children() 	{ return children.size(); }
+	const TreeNode* get_child(int i) 	{ return children[i]; }
+	const& Board 	get_board() 		{ return board; }
+	Board 		get_board_copy() 	{ return board; }
+	NodeType 	get_node_type() 	{ return node_type; }
+	Move 		get_move() 		{ return move; }
 
+	void removeChild(int i) { children.erase(children.begin()+i); }
 	void createChildren(int depth);
+	void extend();
 
 private:
 	std::vector<TreeNode*> children;
@@ -70,6 +75,35 @@ private:
 	Nodetype node_type;
 };
 
+void Tree::pruneTree(TreeNode* child) {
+	// already checked to see if a child exists in the gen move function
+	for(int i = 0; i<children.size(); i++) {
+		if(head->get_child()==child) head->removeChild(i);
+	}
+	delete head;
+	head = child;
+}
+
+TreeNode::TreeNode(TreeNode* h) {
+	board = h->get_board_copy();
+	move = h->get_move();
+	node_type = h->get_node_type();
+	for(int i = 0; i< h->number_of_children(); i++) {
+		TreeNode* child = new Treenode(h->getChild(i));
+		children.push_back(child);
+	}
+}
+
+Tree::TreeNode(Board b, NodeType n) {
+	board = b;
+	node_type = n;
+}
+
+Tree::TreeNode(Board b, NodeType n, move m) {
+	TreeNode(b,n);
+	move = m;
+}
+
 TreeNode::~TreeNode() {
 	for(int i = 0; i<children.size(); i++) {
 		delete children[i];
@@ -78,13 +112,14 @@ TreeNode::~TreeNode() {
 
 void Tree::createTree(const Board& h, int depth) {
 	head = new TreeNode(board, MAX);
-	head.createChildren(depth);
+	head->createChildren(depth);
 }
 
 void TreeNode::createChildren(int depth) {
 	if(depth>0) {
-		NodeType node_type WRITE SOEM FUNTION OF THIS;
-	
+		NodeType n_type;
+		if(node_type==MAX) n_type = MIN;
+		else n_type = MAX;
 		for(int i = 0; i<15; i++) {
 			for(int j = 0; j<15; j++) {
 				if((	board(i+1,j)
@@ -96,11 +131,32 @@ void TreeNode::createChildren(int depth) {
 				||	board(i-1,j+1)
 				||	board(i-1,j-1))
 				&& 	board(i,j)==EMPTY ) {
-					board(i,j) = WHITE;
-					head = new TreeNode(board, node_type, Move(i,j,WHITE) );
-					head.createChildren(depth-1);
+					if(node_type==MAX) board(i,j) = WHITE;
+					else board(i,j) = BLACK;
+					TreeNode* child = new TreeNode(board, n_type, Move(i,j,board(i,j)));
+					children.push_back(child);
+					board(i,j) = EMPTY;
+					child->createChildren(depth-1);
 				}
 			}
 		}
 	}
 }
+
+void Tree::extendTree() {
+	head->extend();
+}
+
+void TreeNode::extend() {
+	if(children.size()==0) createChildren(2);
+	else {
+		for(int i = 0; i<children.size(); i++) {
+			children[i]->extend();
+		}
+	}
+}
+/*
+void Tree::calculateTree() {
+	
+}
+*/
